9ca21c58bc6e070f97b5b98f4043803933f7420b
Yoows <xolsp123@gmail.com>
2021-06-04 10:19:51 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import Navbar from "./components/navbar";
+import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
+import "./App.css";
+import RegisterForm from './components/common/registerForm';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm}></Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/register" component={RegisterForm}></Route>
+          <Route path="/login" component={LoginForm}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="movies"></Redirect>
+          <Redirect to="/not-found"></Redirect>
+        </Switch>
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
